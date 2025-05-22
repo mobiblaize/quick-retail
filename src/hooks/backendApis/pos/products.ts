f5459f1b9a4d49498e2b3e52b2e312304a4038c5
Babatunde Atijosan <atijosanbabatunde@gmail.com>
2025-05-22 22:34:30 +0100

    fix product table

--- src/hooks/backendApis/pos/products.ts
+++ src/hooks/backendApis/pos/products.ts
@@ -8,12 +8,29 @@ import { useFetchPostData, usePostData } from "../../useApis";
     paginate: true,
   };
 
+  const defaultsPayload = {
+    search: "",
+    sort_by: "recent",
+   location_name: "ikeja city mall",
+    category_name: "",
+    price_from: "",
+    price_to: "",
+    date_range: "",
+    per_page: "",
+    paginate: true,
+  };
+
 export const useCreateProduct = () => {
   return usePostData("pos/product/add-product");
 };
 
-export const getAllProduct = () => {
-  return usePostData("pos/product/all");
+export const useFetchAllProducts = (
+  productPayload?: Partial<typeof defaultsPayload>
+) => {
+
+  const payload = { ...defaultsPayload, ...productPayload };
+
+  return useFetchPostData("pos/product/all", payload);
 };
 
 export const getAllLocation = () => {
