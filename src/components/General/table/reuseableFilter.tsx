d381a23d36ba51f8e4013b0c4c40e8a364ddf07f
Tobbie91 <oluwatobiakinrimisi@gmail.com>
2025-08-04 14:59:21 +0100

    build fix

--- src/components/General/table/reuseableFilter.tsx
+++ src/components/General/table/reuseableFilter.tsx
@@ -72,9 +72,7 @@ const ReusableFilterComponent: React.FC<ReusableFilterComponentProps> = ({
   showRole = false,
   showModule = false,
   setFiltersApplied,
-  setAppliedFilters,
 
-  onResetFilter,
 }) => {
   const [filters, setFilters] = useState<FilterValues>({
     startDate: '',
@@ -124,7 +122,7 @@ const ReusableFilterComponent: React.FC<ReusableFilterComponentProps> = ({
   
   useEffect(() => {
     const hasFilters = Object.entries(filters).some(
-      ([key, val]) => val && val !== '' && val !== 'All' && val !== 'all'
+      ([ val]) => val && val !== '' && val !== 'All' && val !== 'all'
     );
   
     setFiltersApplied?.(hasFilters);
