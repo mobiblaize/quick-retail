c44e6a4b09485a31c5da61cbd33faa4149a7bdf9
Tobbie91 <oluwatobiakinrimisi@gmail.com>
2025-08-11 23:26:34 +0100

    subscription, customer, iscount fix

--- src/components/dashboard/adminPage/vendor/paymentSuccessfulModal.tsx
+++ src/components/dashboard/adminPage/vendor/paymentSuccessfulModal.tsx
@@ -1,22 +1,43 @@
-import { Modal, Button } from '@mantine/core';
-import { CheckCircle } from 'lucide-react';
-
+import { Modal, Button } from "@mantine/core";
+import { CheckCircle } from "lucide-react";
 
 interface PaymentSuccessModalProps {
-    opened: boolean;
-    onClose: () => void;
+  opened: boolean;
+  onClose: () => void;
+  reference?: string;
+  email?: string;
+  loading?: boolean;
+}
 
-  }
-
-export default function PaymentSuccessModal({ opened, onClose }: PaymentSuccessModalProps) {
+export default function PaymentSuccessModal({
+  opened,
+  onClose,
+  reference,
+}: PaymentSuccessModalProps) {
+  
   return (
-    <Modal opened={opened} onClose={onClose} withCloseButton={false} centered radius="md" size="sm" padding={0}>
+    <Modal
+      opened={opened}
+      onClose={onClose}
+      withCloseButton={false}
+      centered
+      radius="md"
+      size="sm"
+      padding={0}
+    >
       <div className="bg-white p-6 rounded-lg flex flex-col items-center text-center">
         <CheckCircle className="text-green-500 w-12 h-12 mb-4 animate-pulse" />
         <h2 className="text-lg font-semibold">Subscription Successful</h2>
-        <p className="text-sm text-gray-500 mt-2 mb-6">
-          You have successfully changed your subscription plan. You can choose to renew this plan or change plan later.
+        <p className="text-sm text-gray-500 mt-2 mb-4">
+          You have successfully changed your <b/> subscription plan. You can
+          choose to renew <b/> this plan or change plan later.
         </p>
+
+        {reference && (
+          <div className=" text-gray-700 text-sm p-2 rounded mb-6 w-full">
+            {/* <strong>Reference:</strong> {reference} */}
+          </div>
+        )}
 
         <Button
           fullWidth
