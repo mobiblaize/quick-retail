e221f0a1081e8c57a1a37e7da1308d47bcbb0fa0
Oni Abdullahi Oluwabusayo <busayaooni@gmail.com>
2025-10-08 15:21:29 +0100

    Refactor code structure for improved readability and maintainability

--- src/components/dashboard/pointOfSales/inventoryManagement/inventoryTable.tsx
+++ src/components/dashboard/pointOfSales/inventoryManagement/inventoryTable.tsx
@@ -207,7 +207,7 @@ if (appliedFilters.orderStatus) {
       render: (row: any) => (
         <Link to={ROUTES.updateInventory} state={{ inventories: row.original }}>
           <Text fw={700} c="customPrimary.10" className="cursor-pointer">
-            Reorder
+            Update
           </Text>
         </Link>
       ),
