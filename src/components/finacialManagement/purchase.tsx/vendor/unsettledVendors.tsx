d4383b8772403a3a374e32b6c8f32adf6d515321
Emmanuel Asimobi <asimobiemmanuel@gmail.com>
2025-10-08 15:35:34 +0100

    Fixes form input

--- src/components/finacialManagement/purchase.tsx/vendor/unsettledVendors.tsx
+++ src/components/finacialManagement/purchase.tsx/vendor/unsettledVendors.tsx
@@ -86,8 +86,7 @@ const UnsettledVendorTable = () => {
               paddingY={6}
               bgColor="#D0D5DD"
               className="w-[128px] rounded-lg"
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>  {
-                const value = e.target.value;
+              onChange={(value: string)  => {
 
                 if (value.trim()) {
                   navigate(ROUTES.filledUnsettledVendor);
